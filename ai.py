c7fe0847a82edd84af3e6a9df2b3d683f185a058
Boolean Nguyen <bvn006@ucsd.edu>
2018-01-31 05:34:12 -0800

    starting

--- ai.py
+++ ai.py
@@ -6,7 +6,12 @@ class State:
 	"""game state information"""
 	#Hint: probably need the tile matrix, which player's turn, score, previous move
 	def __init__(self, matrix, player, score, pre_move):
-		pass
+		self.matrix = matrix
+		self.player = player
+		self.score = score
+		self.pre_move = pre_move
+
+		
 	def highest_tile(self):
 		"""Return the highest tile here (just a suggestion, you don't have to)"""
 		pass
@@ -45,14 +50,63 @@ class Simulator:
 	def move(self, direction):
 		pass
 	def moveTiles(self):
-		pass
+		tm = self.tileMatrix
+		for i in range(0, self.board_size):
+			for j in range(0, self.board_size - 1):
+				while tm[i][j] == 0 and sum(tm[i][j:]) > 0:
+					for k in range(j, self.board_size - 1):
+						tm[i][k] = tm[i][k + 1]
+					tm[i][self.board_size - 1] = 0
+		
 	def mergeTiles(self):
-		pass
+		tm = self.tileMatrix
+		for i in range(0, self.board_size):
+			for k in range(0, self.board_size - 1):
+				if tm[i][k] == tm[i][k + 1] and tm[i][k] != 0:
+					tm[i][k] = tm[i][k] * 2
+					tm[i][k + 1] = 0
+					self.total_points += tm[i][k]
+					self.moveTiles()
+		
 	def checkIfCanGo(self):
-		pass
+		tm = self.tileMatrix
+		for i in range(0, self.board_size ** 2):
+			if tm[int(i / self.board_size)][i % self.board_size] == 0:
+				return True		
+		for i in range(0, self.board_size):
+			for j in range(0, self.board_size - 1):
+				if tm[i][j] == tm[i][j + 1]:
+					return True
+				elif tm[j][i] == tm[j + 1][i]:
+					return True
+		return False
+		
 	def canMove(self):
-		pass
+		tm = self.tileMatrix
+		for i in range(0, self.board_size):
+			for j in range(1, self.board_size):
+				if tm[i][j-1] == 0 and tm[i][j] > 0:
+					return True
+				elif (tm[i][j-1] == tm[i][j]) and tm[i][j-1] != 0:
+					return True
+		return False
+		
 	def rotateMatrixClockwise(self):	
-		pass		
+		tm = self.tileMatrix
+		for i in range(0, int(self.board_size/2)):
+			for k in range(i, self.board_size- i - 1):
+				temp1 = tm[i][k]
+				temp2 = tm[self.board_size - 1 - k][i]
+				temp3 = tm[self.board_size - 1 - i][self.board_size - 1 - k]
+				temp4 = tm[k][self.board_size - 1 - i]
+				tm[self.board_size - 1 - k][i] = temp1
+				tm[self.board_size - 1 - i][self.board_size - 1 - k] = temp2
+				tm[k][self.board_size - 1 - i] = temp3
+				tm[i][k] = temp4	
 	def convertToLinearMatrix(self):
-		pass
+		m = []
+		for i in range(0, self.board_size ** 2):
+			m.append(self.tileMatrix[int(i / self.board_size)][i % self.board_size])
+		m.append(self.total_points)
+		return m
+		
